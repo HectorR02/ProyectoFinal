b160b0cf82f4a3394f321bbec6b0a6b978968ebd
HectorR02 <engel-t@outlook.es>
2016-09-24 15:02:22 -0400

    Evento para los textBox y el checkBox (Ventana Registro de Usuario)

--- RegistroUsuario/RegistroUsuario.Designer.cs
+++ RegistroUsuario/RegistroUsuario.Designer.cs
@@ -31,15 +31,16 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RegistroUsuario));
             this.PBFondo = new System.Windows.Forms.PictureBox();
             this.Nombre = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.TBnombre = new System.Windows.Forms.TextBox();
+            this.TBUsuario = new System.Windows.Forms.TextBox();
+            this.TBPass = new System.Windows.Forms.TextBox();
+            this.TBConfPass = new System.Windows.Forms.TextBox();
             this.Usuario = new System.Windows.Forms.Label();
             this.Pass = new System.Windows.Forms.Label();
             this.ConfPass = new System.Windows.Forms.Label();
             this.CrearCuenta = new System.Windows.Forms.Button();
             this.Titulo = new System.Windows.Forms.Label();
+            this.CBVerPass = new System.Windows.Forms.CheckBox();
             ((System.ComponentModel.ISupportInitialize)(this.PBFondo)).BeginInit();
             this.SuspendLayout();
             // 
@@ -48,7 +49,7 @@
             this.PBFondo.Image = ((System.Drawing.Image)(resources.GetObject("PBFondo.Image")));
             this.PBFondo.Location = new System.Drawing.Point(1, 1);
             this.PBFondo.Name = "PBFondo";
-            this.PBFondo.Size = new System.Drawing.Size(585, 509);
+            this.PBFondo.Size = new System.Drawing.Size(585, 526);
             this.PBFondo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.PBFondo.TabIndex = 0;
             this.PBFondo.TabStop = false;
@@ -63,41 +64,53 @@
             this.Nombre.TabIndex = 1;
             this.Nombre.Text = "Nombre";
             // 
-            // textBox1
+            // TBnombre
             // 
-            this.textBox1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(107, 133);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(369, 29);
-            this.textBox1.TabIndex = 2;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.TBnombre.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.TBnombre.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TBnombre.ForeColor = System.Drawing.Color.Silver;
+            this.TBnombre.Location = new System.Drawing.Point(107, 133);
+            this.TBnombre.Name = "TBnombre";
+            this.TBnombre.Size = new System.Drawing.Size(369, 29);
+            this.TBnombre.TabIndex = 2;
+            this.TBnombre.Text = "Ejemplo: Juan Perez";
+            this.TBnombre.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TBnombre_KeyPress);
             // 
-            // textBox2
+            // TBUsuario
             // 
-            this.textBox2.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.Location = new System.Drawing.Point(107, 213);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(369, 29);
-            this.textBox2.TabIndex = 3;
-            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            this.TBUsuario.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.TBUsuario.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TBUsuario.ForeColor = System.Drawing.Color.Silver;
+            this.TBUsuario.Location = new System.Drawing.Point(107, 213);
+            this.TBUsuario.Name = "TBUsuario";
+            this.TBUsuario.Size = new System.Drawing.Size(369, 29);
+            this.TBUsuario.TabIndex = 3;
+            this.TBUsuario.Text = "Ejemplo: juan02";
+            this.TBUsuario.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
             // 
-            // textBox3
+            // TBPass
             // 
-            this.textBox3.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox3.Location = new System.Drawing.Point(107, 293);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(369, 29);
-            this.textBox3.TabIndex = 4;
-            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            this.TBPass.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.TBPass.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TBPass.ForeColor = System.Drawing.Color.Silver;
+            this.TBPass.Location = new System.Drawing.Point(107, 293);
+            this.TBPass.Name = "TBPass";
+            this.TBPass.Size = new System.Drawing.Size(369, 29);
+            this.TBPass.TabIndex = 4;
+            this.TBPass.Text = "Contraseña";
+            this.TBPass.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox3_KeyPress);
             // 
-            // textBox4
+            // TBConfPass
             // 
-            this.textBox4.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox4.Location = new System.Drawing.Point(107, 373);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(369, 29);
-            this.textBox4.TabIndex = 5;
-            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            this.TBConfPass.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.TBConfPass.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TBConfPass.ForeColor = System.Drawing.Color.Silver;
+            this.TBConfPass.Location = new System.Drawing.Point(107, 373);
+            this.TBConfPass.Name = "TBConfPass";
+            this.TBConfPass.Size = new System.Drawing.Size(369, 29);
+            this.TBConfPass.TabIndex = 5;
+            this.TBConfPass.Text = "Contraseña";
+            this.TBConfPass.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox4_KeyPress);
             // 
             // Usuario
             // 
@@ -136,13 +149,12 @@
             this.CrearCuenta.FlatAppearance.BorderSize = 0;
             this.CrearCuenta.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.CrearCuenta.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.CrearCuenta.Location = new System.Drawing.Point(107, 431);
+            this.CrearCuenta.Location = new System.Drawing.Point(107, 466);
             this.CrearCuenta.Name = "CrearCuenta";
             this.CrearCuenta.Size = new System.Drawing.Size(369, 29);
             this.CrearCuenta.TabIndex = 9;
             this.CrearCuenta.Text = "Crear Cuenta";
             this.CrearCuenta.UseVisualStyleBackColor = false;
-            this.CrearCuenta.Click += new System.EventHandler(this.button1_Click);
             // 
             // Titulo
             // 
@@ -154,25 +166,38 @@
             this.Titulo.TabIndex = 10;
             this.Titulo.Text = "Crear una Cuenta";
             // 
+            // CBVerPass
+            // 
+            this.CBVerPass.AutoSize = true;
+            this.CBVerPass.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CBVerPass.Location = new System.Drawing.Point(226, 425);
+            this.CBVerPass.Name = "CBVerPass";
+            this.CBVerPass.Size = new System.Drawing.Size(137, 19);
+            this.CBVerPass.TabIndex = 11;
+            this.CBVerPass.Text = "Mostrar Contraseña";
+            this.CBVerPass.UseVisualStyleBackColor = true;
+            this.CBVerPass.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
             // RegistroUsuario
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(587, 510);
+            this.ClientSize = new System.Drawing.Size(587, 527);
+            this.Controls.Add(this.CBVerPass);
             this.Controls.Add(this.Titulo);
             this.Controls.Add(this.CrearCuenta);
             this.Controls.Add(this.ConfPass);
             this.Controls.Add(this.Pass);
             this.Controls.Add(this.Usuario);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.TBConfPass);
+            this.Controls.Add(this.TBPass);
+            this.Controls.Add(this.TBUsuario);
+            this.Controls.Add(this.TBnombre);
             this.Controls.Add(this.Nombre);
             this.Controls.Add(this.PBFondo);
             this.MaximizeBox = false;
-            this.MaximumSize = new System.Drawing.Size(603, 549);
-            this.MinimumSize = new System.Drawing.Size(603, 549);
+            this.MaximumSize = new System.Drawing.Size(603, 566);
+            this.MinimumSize = new System.Drawing.Size(603, 566);
             this.Name = "RegistroUsuario";
             this.Text = "Registro de Usuario";
             ((System.ComponentModel.ISupportInitialize)(this.PBFondo)).EndInit();
@@ -185,15 +210,16 @@
 
         private System.Windows.Forms.PictureBox PBFondo;
         private System.Windows.Forms.Label Nombre;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox TBnombre;
+        private System.Windows.Forms.TextBox TBUsuario;
+        private System.Windows.Forms.TextBox TBPass;
+        private System.Windows.Forms.TextBox TBConfPass;
         private System.Windows.Forms.Label Usuario;
         private System.Windows.Forms.Label Pass;
         private System.Windows.Forms.Label ConfPass;
         private System.Windows.Forms.Button CrearCuenta;
         private System.Windows.Forms.Label Titulo;
+        private System.Windows.Forms.CheckBox CBVerPass;
     }
 }
 
--- RegistroUsuario/RegistroUsuario.cs
+++ RegistroUsuario/RegistroUsuario.cs
@@ -16,38 +16,105 @@ namespace RegistroUsuario
         {
             InitializeComponent();
 
-            Nombre.Parent = Usuario.Parent = Pass.Parent = ConfPass.Parent = Titulo.Parent = PBFondo;
-            Nombre.BackColor = Usuario.BackColor = Pass.BackColor = ConfPass.BackColor = Titulo.BackColor = Color.Transparent;
+            Nombre.Parent = Usuario.Parent = Pass.Parent = ConfPass.Parent = Titulo.Parent = CBVerPass.Parent = PBFondo;
+            Nombre.BackColor = Usuario.BackColor = Pass.BackColor = ConfPass.BackColor = Titulo.BackColor = CBVerPass.BackColor = Color.Transparent;
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
         {
-
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                if (string.IsNullOrEmpty(TBUsuario.Text))
+                {
+                    TBUsuario.Text = "Ejemplo: juan02";
+                    TBUsuario.ForeColor = Color.Silver;
+                }
+                TBPass.Focus();
+            }
+            else
+                if (Char.IsDigit(e.KeyChar) || Char.IsLetter(e.KeyChar))
+                if (TBUsuario.Text.Equals("Ejemplo: juan02"))
+                {
+                    TBUsuario.Clear();
+                    TBUsuario.ForeColor = Color.Black;
+                }
         }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
+        private void textBox3_KeyPress(object sender, KeyPressEventArgs e)
         {
-
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                if (string.IsNullOrEmpty(TBPass.Text))
+                {
+                    TBPass.Text = "Contraseña";
+                    TBPass.ForeColor = Color.Silver;
+                    if (TBPass.PasswordChar == '*')
+                        TBPass.PasswordChar = '\0';
+                }
+                TBConfPass.Focus();
+            }
+            else
+                if (Char.IsDigit(e.KeyChar) || Char.IsLetter(e.KeyChar))
+                if (TBPass.Text.Equals("Contraseña"))
+                {
+                    TBPass.Clear();
+                    TBPass.PasswordChar = '*';
+                    TBPass.ForeColor = Color.Black;
+                }
         }
 
-        private void textBox2_TextChanged(object sender, EventArgs e)
+        private void textBox4_KeyPress(object sender, KeyPressEventArgs e)
         {
-
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                if (string.IsNullOrEmpty(TBConfPass.Text))
+                {
+                    TBConfPass.Text = "Contraseña";
+                    TBConfPass.ForeColor = Color.Silver;
+                    if (TBConfPass.PasswordChar == '*')
+                        TBConfPass.PasswordChar = '\0';
+                }
+                TBConfPass.Focus();
+            }
+            else
+                if (Char.IsDigit(e.KeyChar) || Char.IsLetter(e.KeyChar))
+                if (TBConfPass.Text.Equals("Contraseña"))
+                {
+                    TBConfPass.Clear();
+                    TBConfPass.PasswordChar = '*';
+                    TBConfPass.ForeColor = Color.Black;
+                }
         }
 
-        private void textBox3_TextChanged(object sender, EventArgs e)
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
         {
-
+            if (CBVerPass.Checked)
+            {
+                if (TBConfPass.PasswordChar == '*' || TBPass.PasswordChar == '*')
+                    TBPass.PasswordChar = TBConfPass.PasswordChar = '\0';
+            }
+            else
+                TBPass.PasswordChar = TBConfPass.PasswordChar = '*';
         }
 
-        private void textBox4_TextChanged(object sender, EventArgs e)
+        private void TBnombre_KeyPress(object sender, KeyPressEventArgs e)
         {
-
-        }
-
-        private void RegistroUsuario_Load(object sender, EventArgs e)
-        {
-
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                if (string.IsNullOrEmpty(TBnombre.Text))
+                {
+                    TBnombre.Text = "Ejemplo: Juan Perez";
+                    TBnombre.ForeColor = Color.Silver;
+                }
+                TBUsuario.Focus();
+            }
+            else
+                if (Char.IsDigit(e.KeyChar) || Char.IsLetter(e.KeyChar))
+                if (TBnombre.Text.Equals("Ejemplo: Juan Perez"))
+                {
+                    TBnombre.Clear();
+                    TBnombre.ForeColor = Color.Black;
+                }
         }
     }
 }
