fbdf7d9e8ef532523295680710195146904a4d66
HectorR02 <engel-t@outlook.es>
2016-09-23 23:54:18 -0400

    Proyecto Final (Login)

---
+++ Login/Login.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace Login
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
+            this.PBFondoLogin = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.BIniciarSesion = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.PBFondoLogin)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PBFondoLogin
+            // 
+            this.PBFondoLogin.Image = ((System.Drawing.Image)(resources.GetObject("PBFondoLogin.Image")));
+            this.PBFondoLogin.Location = new System.Drawing.Point(3, 3);
+            this.PBFondoLogin.Name = "PBFondoLogin";
+            this.PBFondoLogin.Size = new System.Drawing.Size(411, 325);
+            this.PBFondoLogin.TabIndex = 0;
+            this.PBFondoLogin.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.Color.AntiqueWhite;
+            this.textBox1.Location = new System.Drawing.Point(96, 98);
+            this.textBox1.MaxLength = 15;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(219, 32);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.Text = "Usuario";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(96, 170);
+            this.textBox2.MaxLength = 12;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.PasswordChar = '*';
+            this.textBox2.Size = new System.Drawing.Size(219, 31);
+            this.textBox2.TabIndex = 7;
+            this.textBox2.Text = "Contraseña";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // BIniciarSesion
+            // 
+            this.BIniciarSesion.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BIniciarSesion.Font = new System.Drawing.Font("Goudy Old Style", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BIniciarSesion.Location = new System.Drawing.Point(96, 247);
+            this.BIniciarSesion.Name = "BIniciarSesion";
+            this.BIniciarSesion.Size = new System.Drawing.Size(219, 40);
+            this.BIniciarSesion.TabIndex = 8;
+            this.BIniciarSesion.Text = "Iniciar Sesion";
+            this.BIniciarSesion.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.Font = new System.Drawing.Font("Goudy Old Style", 32.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(73, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(259, 51);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Iniciar Sesión";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(417, 329);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BIniciarSesion);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.PBFondoLogin);
+            this.Name = "Form1";
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.PBFondoLogin)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox PBFondoLogin;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button BIniciarSesion;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Login/Login.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Login
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+            PBFondoLogin.SizeMode = PictureBoxSizeMode.StretchImage;
+            label1.Parent = PBFondoLogin;
+            label1.BackColor = Color.Transparent;
+            if (!textBox1.Focused)
+                textBox1.Clear();
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Usuario_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }        
+    }
+}
