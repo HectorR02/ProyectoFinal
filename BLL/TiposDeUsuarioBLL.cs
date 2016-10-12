43259a1862f4b440cb37b638b1f1526fe83624b9
HectorR02 <engel-t@outlook.es>
2016-10-12 09:30:08 -0400

    clasesBLL para el manejo de la BD

---
+++ BLL/TiposDeUsuarioBLL.cs
@@ -0,0 +1,81 @@
+﻿using DAL;
+using Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class TiposDeUsuarioBLL
+    {
+        public static bool Insertar(TiposDeUsuario nuevo)
+        {
+            bool resultado = false;
+            using (var conexion = new FinalProyectDB())
+            {
+                try
+                {
+                    conexion.TypeUsers.Add(nuevo);
+                    conexion.SaveChanges();
+                    resultado = true;
+                }catch(Exception)
+                {
+                    throw;
+                }
+            }
+            return resultado;
+        }
+        public static TiposDeUsuario Buscar(int TipoDeUsuarioId)
+        {
+            var typeUser = new TiposDeUsuario();
+            using (var conexion = new FinalProyectDB())
+            {
+                try
+                {
+                    typeUser = conexion.TypeUsers.Find(TipoDeUsuarioId);
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+            return typeUser;
+        }
+        public static bool Eliminar(TiposDeUsuario existente)
+        {
+            bool resultado = false;
+            using (var conexion = new FinalProyectDB())
+            {
+                try
+                {
+                    conexion.Entry(existente).State = EntityState.Deleted;
+                    conexion.SaveChanges();
+                    resultado = true;
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+            return resultado;
+        }
+        public static List<TiposDeUsuario> GetLista()
+        {
+            var lista = new List<TiposDeUsuario>();
+            using (var conexion = new FinalProyectDB())
+            {
+                try
+                {
+                    lista = conexion.TypeUsers.ToList();
+                }catch(Exception)
+                {
+                    throw;
+                }
+            }
+            return lista;
+        }
+    }
+}
---
+++ BLL/UsuariosBLL.cs
@@ -0,0 +1,81 @@
+﻿using DAL;
+using Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class UsuariosBLL
+    {
+        public static bool Insertar(Usuarios nuevo)
+        {
+            bool resultado = false;
+            using (var conexion = new FinalProyectDB())
+            {
+                try
+                {
+                    conexion.Users.Add(nuevo);
+                    conexion.SaveChanges();
+                    resultado = true;
+                }catch(Exception)
+                {
+                    throw;
+                }
+            }
+            return resultado;
+        }
+        public static Usuarios Buscar(int UsuarioId)
+        {
+            var usuario = new Usuarios();
+            using (var conexion = new FinalProyectDB())
+            {
+                try
+                {
+                    usuario = conexion.Users.Find(UsuarioId);
+                }catch(Exception)
+                {
+                    throw;
+                }
+            }
+            return usuario;
+        }
+        public static bool Eliminar(Usuarios existente)
+        {
+            bool resultado = false;
+            using (var conexion = new FinalProyectDB())
+            {
+                try
+                {
+                    conexion.Entry(existente).State = EntityState.Deleted;
+                    conexion.SaveChanges();
+                    resultado = true;
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+            return resultado;
+        }
+        public static List<Usuarios> GetList()
+        {
+            var lista = new List<Usuarios>();
+            using (var conexion = new FinalProyectDB())
+            {
+                try
+                {
+                    lista = conexion.Users.ToList();
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+            return lista;
+        }
+    }
+}
